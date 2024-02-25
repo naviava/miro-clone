ffc34d159bdc3acc06a1c3083fff8d3a827857c2
Navin Avadhani <knavinavadhani@gmail.com>
2024-02-25 15:52:09 +0530

    Added empty state illustrations.

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -11,5 +11,17 @@ interface IProps {
 export function BoardList({ orgId, query }: IProps) {
   const data = []; //TODO: Change to API call
 
+  if (!data?.length && !!query.search) {
+    return <div>Try searching for something else</div>;
+  }
+
+  if (!data?.length && query.favorites) {
+    return <div>No favorites</div>;
+  }
+
+  if (!data?.length) {
+    return <div>No boards found</div>;
+  }
+
   return <div>{JSON.stringify(query)}</div>;
 }
